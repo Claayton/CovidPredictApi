9f4f03fc2f0fa67126e4f04139b48c5b618b9b3f
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-11-24 12:08:03 -0300

    feat: Adicionado modulo de database, onde sera administrado o db com ORM SQLalchemy

---
+++ app/database/countries.py
@@ -0,0 +1,24 @@
+"""Lista de todos os países da pesquisa"""
+
+all_countries = [
+    'AFG', 'OWID_AFR', 'ALB', 'DZA', 'AND', 'AGO', 'AIA', 'ATG', 'ARG', 'ARM', 'ABW',\
+    'OWID_ASI', 'AUS', 'AUT', 'AZE', 'BHS', 'BHR', 'BGD', 'BRB', 'BLR', 'BEL', 'BLZ', 'BEN',\
+    'BMU', 'BTN', 'BOL', 'BES', 'BIH', 'BWA', 'BRA', 'VGB', 'BRN', 'BGR', 'BFA', 'BDI', 'KHM',\
+    'CMR', 'CAN', 'CPV', 'CYM', 'CAF', 'TCD', 'CHL', 'CHN', 'COL', 'COM', 'COG', 'COK', 'CRI',\
+    'CIV', 'HRV', 'CUB', 'CUW', 'CYP', 'CZE', 'COD', 'DNK', 'DJI', 'DMA', 'DOM', 'ECU', 'EGY',\
+    'SLV', 'GNQ', 'ERI', 'EST', 'SWZ', 'ETH', 'OWID_EUR', 'OWID_EUN', 'FRO', 'FLK', 'FJI',\
+    'FIN', 'FRA', 'PYF', 'GAB', 'GMB', 'GEO', 'DEU', 'GHA', 'GIB', 'GRC', 'GRL', 'GRD', 'GTM',\
+    'GGY', 'GIN', 'GNB', 'GUY', 'HTI', 'OWID_HIC', 'HND', 'HKG', 'HUN', 'ISL', 'IND', 'IDN',\
+    'OWID_INT', 'IRN', 'IRQ', 'IRL', 'IMN', 'ISR', 'ITA', 'JAM', 'JPN', 'JEY', 'JOR', 'KAZ',\
+    'KEN', 'KIR', 'OWID_KOS', 'KWT', 'KGZ', 'LAO', 'LVA', 'LBN', 'LSO', 'LBR', 'LBY', 'LIE',\
+    'LTU', 'OWID_LIC', 'OWID_LMC', 'LUX', 'MAC', 'MDG', 'MWI', 'MYS', 'MDV', 'MLI', 'MLT',\
+    'MHL', 'MRT', 'MUS', 'MEX', 'FSM', 'MDA', 'MCO', 'MNG', 'MNE', 'MSR', 'MAR', 'MOZ', 'MMR',\
+    'NAM', 'NRU', 'NPL', 'NLD', 'NCL', 'NZL', 'NIC', 'NER', 'NGA', 'NIU', 'OWID_NAM', 'MKD',\
+    'OWID_CYN', 'NOR', 'OWID_OCE', 'OMN', 'PAK', 'PLW', 'PSE', 'PAN', 'PNG', 'PRY', 'PER',\
+    'PHL', 'PCN', 'POL', 'PRT', 'QAT', 'ROU', 'RUS', 'RWA', 'SHN', 'KNA', 'LCA', 'VCT', 'WSM',\
+    'SMR', 'STP', 'SAU', 'SEN', 'SRB', 'SYC', 'SLE', 'SGP', 'SXM', 'SVK', 'SVN', 'SLB', 'SOM',\
+    'ZAF', 'OWID_SAM', 'KOR', 'SSD', 'ESP', 'LKA', 'SDN', 'SUR', 'SWE', 'CHE', 'SYR', 'TWN',\
+    'TJK', 'TZA', 'THA', 'TLS', 'TGO', 'TKL', 'TON', 'TTO', 'TUN', 'TUR', 'TKM', 'TCA', 'TUV',\
+    'UGA', 'UKR', 'ARE', 'GBR', 'USA', 'OWID_UMC', 'URY', 'UZB', 'VUT', 'VAT', 'VEN', 'VNM',\
+    'WLF', 'OWID_WRL', 'YEM', 'ZMB', 'ZWE'
+    ]
---
+++ app/database/tables.py
@@ -0,0 +1,39 @@
+"tables"
+
+from sqlalchemy import create_engine, Column, Integer, Date
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///app/database/database.db', echo=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class CovidBrazil(Base):
+    """Dados sobre o covid no brasil nos ultimos 600 dias."""
+    __tablename__ = 'covid_brazil'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    new_cases = Column(Integer)
+
+    def __repr__(self):
+        return f"Date {self.date}"
+
+
+class CovidWorld(Base):
+    """Dados sobre o covid no mundo nos ultimos 600 dias."""
+    __tablename__ = 'covid_world'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    new_cases = Column(Integer)
+
+    def __repr__(self):
+        return f"Date {self.date}"
+
+
+Base.metadata.create_all(engine)

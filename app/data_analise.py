94a2625ce1f19fcea2dd0ff0a3fdb77ab700e2a9
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-11-26 14:14:18 -0300

    refactor: reorganizado o codigo utilizando classes

--- app/data_analise.py
+++ app/data_analise.py
@@ -1,47 +1,65 @@
 """Predicts Covid19"""
+from datetime import timedelta
 from math import sqrt
 import pandas as pd
 from numpy import mean
 from sklearn.metrics import mean_squared_error
-from fetch_data import read_data_from_brazil
-from datetime import datetime, timedelta
+from .database.fetch_data import DadosCovid
+from . import config
 
-data = read_data_from_brazil()
+dc = DadosCovid(config.search_url, "BRA")
+data = dc.read_data_from_brazil()
 
-def adicionar_dias_ao_data_frame(df, dias):
-    current_day = df[-1][0]
-    for c in range(0, dias):
-        current_day += timedelta(days = 1)
-        df.append((current_day, 0))
 
-adicionar_dias_ao_data_frame(data, 5)
+class AnalizadorDeCovid():
+    """Realizar a analise dos dados coletados do Covid-19"""
 
-data_frame = pd.DataFrame(data)
+    def __init__(self, data_base, days):
+        self.data_base = data_base
+        self.days = days
 
-print(data_frame)
+    def adicionar_dias_ao_data_frame(self):
+        """Adiciona os dias a serem previstos"""
+        current_day = self.data_base[-1][0]
+        count = 0
+        while count < self.days:
+            current_day += timedelta(days = 1)
+            self.data_base.append((current_day, -1))
+            count += 1
 
-def vidente_carlinhos():
-    """
-    Realiza a previsão de casos de covid no proximo dia,
-    com base na média de casos de dois dias atrás.
-    """
-    X = data_frame.values
-    window = 2
-    history = [X[i][1] for i in range(window)]
-    test = [X[i][1] for i in range(window, len(X))]
-    predicoes = []
+    def vidente_carlinhos(self):
+        """
+        Realiza a previsão de casos de covid no proximo dia,
+        com base na média de casos de dois dias atrás.
+        """
+        self.adicionar_dias_ao_data_frame()
+        data_frame = pd.DataFrame(self.data_base)
 
-    for t in range(len(test)):
+        X = data_frame.values
+        window = 2
+        history = [X[i][1] for i in range(window)]
+        test = [X[i][1] for i in range(window, len(X))]
+        predicoes = []
 
-        length = len(history)
-        
-        valor_predito = mean([history[i] for i in range(length - window, length)])
-        valor_real = test[t]
-        predicoes.append(valor_predito)
-        history.append(valor_real)
-        print(f'Dia: {X[t][0]}: Valor predito: {valor_predito:.2f}, Valor real: {valor_real:.2f}')
+        for index, value in enumerate(test):
 
-    rmse = sqrt(mean_squared_error(test, predicoes))  
-    print(f'Metrica de RMSE: {rmse}')
+            length = len(history)
+            valor_predito = mean([history[i] for i in range(length - window, length)])
 
-vidente_carlinhos()
+            if value < 0:
+                test[index] = valor_predito
+                valor_real = test[index]
+            valor_real = test[index]
+            predicoes.append(valor_predito)
+            history.append(valor_real)
+
+            print(f'Dia: {X[index][0]}:\
+                    Valor predito: {valor_predito:.2f},\
+                    Valor real:{valor_real:.2f}')
+
+        rmse = sqrt(mean_squared_error(test, predicoes))
+        print(f'Metrica de RMSE: {rmse}')
+
+
+dados = AnalizadorDeCovid(data, 12)
+dados.vidente_carlinhos()

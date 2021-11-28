0ea399d2cefa78c32e179fc73152c9811e131691
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-11-28 14:09:11 -0300

    feat: Instanciadas as novas funções e metodos, para a execução do programa

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app import config
 from app.interface.app_interface import Interface
 from app.database.fetch_data import DadosCovid
 from app.database.tables import create_database_if_not_exist
+from app.analyzer.data_analise import AnalizadorDeCovid
 
 interface = Interface()
 dc = DadosCovid(config.search_url, "BRA")
@@ -15,10 +16,16 @@ if choice == 0:
     if not data:
         dc.register_data_from_brazil()
         data = dc.read_data_from_brazil()
+    days = interface.read_data_menu('Brasil')
+    ac = AnalizadorDeCovid(data, days)
+    ac.adicionar_dias_ao_data_frame()
+    ac.vidente_carlinhos()
 else:
     data = dc.read_data_from_world()
     if not data:
         dc.register_data_from_world()
         data = dc.read_data_from_world()
-        print('nao tem nada aqui fio')
-    print(data)
+    days = interface.read_data_menu('Mundo')
+    ac = AnalizadorDeCovid(data, days)
+    ac.adicionar_dias_ao_data_frame()
+    ac.vidente_carlinhos()

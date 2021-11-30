9fd52cb7b2677e325f461174285de929f0e92192
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-11-30 13:27:35 -0300

    test: Implementado teste para metodos da classe DadosCovid

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,9 +1,22 @@
 """Testes relacionados a busca de dados na API"""
 import requests
 from app import config
+from app.database.fetch_data import DadosCovid
 
 def test_if_the_search_url_is_working():
     """Test if the search url is working"""
     response = requests.get(config.search_url)
     status = response.status_code
     assert status == 200
+
+def test_if_fetch_data_by_country_is_working():
+    """Test if the search data by country is working"""
+    dc = DadosCovid(config.search_url, "BRA")
+    response = dc.fetch_data_by_country("BRA")
+    assert response[0]['new_cases'] == 1.0
+
+def test_if_fetch_data_from_world_is_working():
+    """Test if the search data from world is working"""
+    dc = DadosCovid(config.search_url, "BRA")
+    response = dc.fetch_data_from_world()
+    assert response[0][0]['new_cases'] == 5.0

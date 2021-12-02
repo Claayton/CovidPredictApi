85f8737dc2aa9465526b73efdf4b7a1134b64eda
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-02 12:15:16 -0300

    feat: Adicionada função dele_arquivo_if_exist + alterações nas docStrings + pequena adição de prints para melhorar o UX

--- app/database/tables.py
+++ app/database/tables.py
@@ -1,28 +1,45 @@
-"tables"
+"Criação de banco de dados e tabelas"
 
 import os.path
+from time import sleep
 from sqlalchemy import create_engine, Column, Integer, Date
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-file = 'app/database/datacovid.db'
-engine = create_engine(f'sqlite:///{file}', echo=True)
+engine = create_engine('sqlite:///app/database/datacovid.db')
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 Base = declarative_base()
 
-def create_database_if_not_exist(file='app/database/datacovid.db'):
+def create_database_if_not_exist(file):
+    """
+    Verifica se o arquivo datacovid.db existe,
+    em caso negativo, cria o arquivo já com as tabelas prontas.
+    """
     if os.path.isfile(file):
-        print('\033[35mBanco de dados já existe\033[m')
+        print(f'\033[35m{"=" * 75}\033[m')
+        print(f'\033[35m{"Arquivo de dados encontrado...":^75}\033[m')
+        sleep(2)
     else:
-        print(f'\033[35mFile {file} does not exist!, creating...\033[m')
+        print(f'\033[35m{"=" * 75}\033[m')
+        print(f'\033[35m{"Arquivo de dados não encontrado, criando arquivo...":^75}\033[m')
+        sleep(2)
         Base.metadata.create_all(engine)
+
+def delete_arquivo_if_exist(file):
+    """
+    Verifica se o aqruivo datacovid.db existe, em caso positivo, deleta o arquivo.
+    """
+    if os.path.isfile(file):
+        os.remove(file)
 
 
 class CovidBrazil(Base):
-    """Dados sobre o covid no brasil nos ultimos 600 dias."""
+    """
+    Tabela de dados sobre o covid-19 no brasil nos ultimos 600 dias aproximadamente.
+    """
     __tablename__ = 'covid_brazil'
 
     id = Column(Integer, primary_key=True)
@@ -34,7 +51,9 @@ class CovidBrazil(Base):
 
 
 class CovidWorld(Base):
-    """Dados sobre o covid no mundo nos ultimos 600 dias."""
+    """
+    Tabela de dados sobre o covid-19 no mundo nos ultimos 600 dias aproximadamente.
+    """
     __tablename__ = 'covid_world'
 
     id = Column(Integer, primary_key=True)

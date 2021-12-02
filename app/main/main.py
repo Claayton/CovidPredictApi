39b07cc0216dff572d1173a6df99fac094d6b975
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-02 13:27:27 -0300

    feat: Adicionado loop para programa continuar rodando a menos que usuário digite 777

--- app/main/main.py
+++ app/main/main.py
@@ -13,34 +13,42 @@ def run():
     interface = Interface()
 
     while True:
-        choice = interface.main_menu()
-        if choice == 0:
-            delete_arquivo_if_exist('app/database/datacovid.db')
-        else:
+        while True:
+            choice = interface.main_menu()
+            if choice == 0:
+                delete_arquivo_if_exist('app/database/datacovid.db')
+            else:
+                break
+        if choice == 777:
+            interface.farewall()
             break
-    dc = DadosCovid(config.search_url, "BRA")
-    if choice == 1:
-        try:
-            data = dc.read_data_from_brazil()
-            if not data:
-                raise ValueError()
-        except (OperationalError, ValueError):
-            dc.register_data_from_brazil()
-            data = dc.read_data_from_brazil()
-        days = interface.read_data_menu('Brasil')
-        ac = CovidAnalyzer(data, days)
-        result = ac.predict_covid_evolution()
-    else:
-        try:
-            data = dc.read_data_from_world()
-            if not data:
-                raise  ValueError()
-        except (OperationalError, ValueError):
-            dc.register_data_from_world()
-            data = dc.read_data_from_world()
-        days = interface.read_data_menu('Mundo')
-        ac = CovidAnalyzer(data, days)
-        result = ac.predict_covid_evolution()
-    for day in result:
-        print(f'Dia: {day["index"]}:\
-                Previsão: {day["predito"]:.2f}')
+        dc = DadosCovid(config.search_url, "BRA")
+        if choice == 1:
+            try:
+                data = dc.read_data_from_brazil()
+                if not data:
+                    raise ValueError()
+            except (OperationalError, ValueError):
+                dc.register_data_from_brazil()
+                data = dc.read_data_from_brazil()
+            days = interface.read_data_menu('Brasil')
+            ac = CovidAnalyzer(data, days)
+            result = ac.predict_covid_evolution()
+        else:
+            try:
+                data = dc.read_data_from_world()
+                if not data:
+                    raise  ValueError()
+            except (OperationalError, ValueError):
+                dc.register_data_from_world()
+                data = dc.read_data_from_world()
+            days = interface.read_data_menu('Mundo')
+            ac = CovidAnalyzer(data, days)
+            result = ac.predict_covid_evolution()
+        for day in result:
+            print(f'Dia: {day["index"]}:\
+                    Previsão: {day["predito"]:.2f}')
+        again = interface.again()
+        if again[0] in 'Nn':
+            interface.farewall()
+            break

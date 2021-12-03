e8221ca10623ab1cf1e2a1fbe42db373624aac43
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-03 11:41:13 -0300

    test: Pequena atualizaçao nos testes, mudança no nome da pasta

--- tests/test_data_analysis.py
+++ tests/test_data_analysis.py
@@ -2,7 +2,7 @@
 from datetime import date
 from app.analyzer.data_analysis import CovidAnalyzer
 
-def test_if_adicionar_dias_ao_data_frame_is_working():
+def test_if_add_days_to_data_frame_is_working():
     """
     Teste para garantir que o sistema consegue adicionar os dias a serem previstos.
     """
@@ -16,19 +16,20 @@ def test_if_adicionar_dias_ao_data_frame_is_working():
     response = ac.add_days_to_data_frame()
     assert response == today
 
-def test_if_vidente_carlinhos_is_working():
+def test_if_predict_covid_evolution_is_working():
     """
     Teste para garantir que a função de analise esta funcionando da forma correta
     """
     data = [
-        (date(2021, 1, 1),69074),
-        (date(2021, 1, 2), 57837),
-        (date(2021, 1, 3), 52383),
-        (date(2021, 1, 4),69074),
-        (date(2021, 1, 5), 57837),
-        (date(2021, 1, 6), 52383),
-        (date(2021, 1, 7),69074),
+        (date(2021, 1, 1),7000),
+        (date(2021, 1, 2), 6000),
+        (date(2021, 1, 3), 5000),
+        (date(2021, 1, 4),4000),
+        (date(2021, 1, 5), 3000),
+        (date(2021, 1, 6), 2000),
+        (date(2021, 1, 7),1000),
     ]
     ac = CovidAnalyzer(data, 7)
-    response = ac.predict_covid_evolution()[0]['predito']
-    assert int(response) == 6052
+    response = ac.predict_covid_evolution()
+    assert 'predito' in response[0]
+    assert int(response[0]['predito']) == 281

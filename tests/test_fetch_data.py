b948a2f0b9ed4fed41e51ad84bc79bb583de5e7e
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-12 17:48:17 -0300

    test: Implementado test de erro para o metodo get_data_covid + requests_mock

--- tests/test_fetch_data.py
+++ tests/test_fetch_data.py
@@ -1,21 +1,19 @@
 """Testes relacionados a busca de dados na API"""
 from src.database.fetch_data import DataCovidConsumer
+from src import config
+from src.errors import HttpRequestError
 
-# def test_url(requests_mock):
-#     requests_mock.get('http://google.com', text='data')
-#     assert 'data' == requests.get('http://google.com').text
-
-def test_get_data_covid():
+def test_get_data_covid(requests_mock):
     """Test if the search data by country is working"""
 
-    # requests_mock.get(
-    #     'https://covid.ourworldindata.org/data/owid-covid-data.json/',
-    #     status_code = 200,
-    #     json = {"some": "thing"}
-    # )
-    url = 'https://covid.ourworldindata.org/data/owid-covid-data.json/'
+    url = config.SEARCH_URL
+    requests_mock.get(
+        url=url,
+        status_code = 200,
+        json = {'some': 'thing', 'BRA': {'data': [{}]}}
+    )
 
-    data_covid_consumer = DataCovidConsumer("BRA")
+    data_covid_consumer = DataCovidConsumer(url)
     get_data_covid_response = data_covid_consumer.get_data_covid()
 
     assert get_data_covid_response.request.method == 'GET'
@@ -23,6 +21,25 @@ def test_get_data_covid():
 
     assert get_data_covid_response.status_code == 200
     assert isinstance(get_data_covid_response.response['BRA']['data'], list)
+
+def test_get_data_covid_http_error(requests_mock):
+    """Teste de erro em get_data_covid"""
+
+    url = f'{config.SEARCH_URL}BRA'
+    requests_mock.get(
+        url=url,
+        status_code = 404,
+        json = {'details': 'somenthing'}
+    )
+
+    data_covid_consumer = DataCovidConsumer(url)
+    try:
+        data_covid_consumer.get_data_covid()
+        assert True is False
+    except HttpRequestError as error:
+        assert error.message is not None
+        assert error.status_code is not None
+
 
 # def test_if_fetch_data_from_world_is_working():
 #     """Test if the search data from world is working"""

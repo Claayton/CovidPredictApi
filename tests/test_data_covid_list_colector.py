6eae03176f3fde8dd66d9808f08f5a6839aa7951
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-12 20:34:14 -0300

    style: Adicionado algumas docstrings no diretorio test_data_covid_list_colector

--- tests/test_data_covid_list_colector.py
+++ tests/test_data_covid_list_colector.py
@@ -1,9 +1,12 @@
-from src.data.usecases.data_covid_list_colector import DataCovidListColector
+"""Testes relacionados a dominios e casos de uso"""
 from src.data.usecases.data_covid_list_colector import DataCovidListColector
 from src.infra import DataCovidConsumer
 from src import config
 
 def test_list():
+    """
+    Testando o coletor da lista de dados covid no mundo.
+    """
     api_consumer = DataCovidConsumer(config.SEARCH_URL)
     data_covid_list_colector = DataCovidListColector(api_consumer)
 

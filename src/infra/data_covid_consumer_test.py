398f6465aa7a6d44833fda174362b255c3962da4
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-13 13:50:06 -0300

    test: Atualizada a localizaçao dos testes + Adicionado parametro "country"

--- src/infra/data_covid_consumer_test.py
+++ src/infra/data_covid_consumer_test.py
@@ -7,13 +7,15 @@ def test_get_data_covid(requests_mock):
     """Test if the search data by country is working"""
 
     url = config.SEARCH_URL
+    country = ['BRA']
+
     requests_mock.get(
         url=url,
         status_code = 200,
-        json = {'some': 'thing', 'BRA': {'data': [{}]}}
+        json = {'some': 'thing', country[0]: {'data': [{}]}}
     )
 
-    data_covid_consumer = DataCovidConsumer(url)
+    data_covid_consumer = DataCovidConsumer(url, country)
     get_data_covid_response = data_covid_consumer.get_data_covid()
 
     assert get_data_covid_response.request.method == 'GET'
@@ -26,23 +28,18 @@ def test_get_data_covid_http_error(requests_mock):
     """Teste de erro em get_data_covid"""
 
     url = f'{config.SEARCH_URL}BRA'
+    country = ['BRA']
+
     requests_mock.get(
         url=url,
         status_code = 404,
         json = {'details': 'somenthing'}
     )
 
-    data_covid_consumer = DataCovidConsumer(url)
+    data_covid_consumer = DataCovidConsumer(url, country)
     try:
         data_covid_consumer.get_data_covid()
         assert True is False
     except HttpRequestError as error:
         assert error.message is not None
         assert error.status_code is not None
-
-
-# def test_if_fetch_data_from_world_is_working():
-#     """Test if the search data from world is working"""
-#     dc = DadosCovid(config.search_url, "BRA")
-#     response = dc.fetch_data_from_world()
-#     assert response[0][0]['new_cases'] == 5.0

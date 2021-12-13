8c016bd0dba33cf69177da67bdfa843ef27e5f79
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-13 13:53:02 -0300

    test: Atualizada a localizaçao dos testes + Atualizaçao no test de coletor de listagem de dados

---
+++ src/data/usecases/data_covid_list_colector_test.py
@@ -0,0 +1,18 @@
+"""Testes relacionados a dominios e casos de uso"""
+from src.data.usecases.data_covid_list_colector import DataCovidListColector
+from src.infra.tests.data_covid_consumer import DataCovidConsumerSpy
+
+def test_list():
+    """
+    Testando o coletor da lista de dados covid no mundo.
+    """
+    api_consumer = DataCovidConsumerSpy()
+    data_covid_list_colector = DataCovidListColector(api_consumer)
+
+    country = ['BRA']
+    response = data_covid_list_colector.list(country)
+
+    assert api_consumer.get_data_covid_attributes == {'country': country}
+    assert isinstance(response, list)
+    assert "id" in response[0]
+    assert "new_cases" in response[0]

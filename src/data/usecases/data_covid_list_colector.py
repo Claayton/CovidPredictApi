7c9267ea751a83a28d1d5021759405956312d83f
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-13 13:59:09 -0300

    feat: Implementado metodo para separar apenas os dados necessarios

--- src/data/usecases/data_covid_list_colector.py
+++ src/data/usecases/data_covid_list_colector.py
@@ -1,5 +1,5 @@
 """Diretório para Dominio e casos de uso"""
-from typing import Dict, Type
+from typing import List, Dict, Type
 from src.domain.usecases import DataCovidListColectorInterface
 from src.data.interfaces.data_covid_consumer import DataCovidConsumerInterface
 
@@ -10,6 +10,27 @@ class DataCovidListColector(DataCovidListColectorInterface):
     def __init__(self, api_consumer: Type[DataCovidConsumerInterface]) -> None:
         self.__api_consumer = api_consumer
 
-    def list(self) -> Dict:
-        response = self.__api_consumer.get_data_covid()
-        print(response)
+    def list(self, country: List[str]) -> List[Dict]:
+        api_response = self.__api_consumer.get_data_covid(country)
+        data_covid_formated_list = self.__format_api_response(
+            api_response.response[country[0]]['data']
+        )
+        return data_covid_formated_list
+
+    @classmethod
+    def __format_api_response(cls, api_response: List[Dict]) -> List[Dict]:
+        data_covid_formated_list = []
+
+        for index, day in enumerate(api_response):
+            try:
+                data_covid_formated_list.append(
+                    {
+                        "id": index,
+                        "date": day["date"],
+                        "new_cases": day["new_cases"]
+                    }
+                )
+            except KeyError:
+                continue
+        return data_covid_formated_list
+  

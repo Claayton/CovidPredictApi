40dda633783c9f660ee2756fa4adb94d0504e757
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-14 13:15:32 -0300

    feat: Iniciada implementaçao de adaptaçoes no frawork

--- src/main/routes/data_covid_routes.py
+++ src/main/routes/data_covid_routes.py
@@ -1,14 +1,16 @@
 """Diretório de rotas do app"""
 from fastapi import APIRouter, Request as RequestFastApi
 from src.validators.get_data_covid_from_country_validator import get_from_country_validator
+from src.main.adapters.request_adapter import request_adapter
 
 data_covid_routes = APIRouter()
 
 @data_covid_routes.get("/api/datacovid/list")
-def get_data_covid_from_country(request: RequestFastApi):
+async def get_data_covid_from_country(request: RequestFastApi):
     """Get_data_covid_from_country"""
 
     get_from_country_validator(request)
+    await request_adapter(request, print)
 
     return {"Ola": "Mundo!"}
  

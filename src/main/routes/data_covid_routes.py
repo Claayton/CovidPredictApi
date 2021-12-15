876be2f7dcec4037096ed29d34923fa20ed8819c
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-15 12:54:24 -0300

    feat: Implementado o composers, onde as classes sao inicialidas e instanciadas

--- src/main/routes/data_covid_routes.py
+++ src/main/routes/data_covid_routes.py
@@ -1,7 +1,12 @@
 """Diretório de rotas do app"""
 from fastapi import APIRouter, Request as RequestFastApi
+from fastapi.responses import JSONResponse
+
 from src.validators.get_data_covid_from_country_validator import get_from_country_validator
 from src.main.adapters.request_adapter import request_adapter
+from src.main.composers.get_data_covid_from_country_composer import (
+    get_data_covid_from_country_composer
+)
 
 data_covid_routes = APIRouter()
 
@@ -10,7 +15,11 @@ async def get_data_covid_from_country(request: RequestFastApi):
     """Get_data_covid_from_country"""
 
     get_from_country_validator(request)
-    await request_adapter(request, print)
+    controller = get_data_covid_from_country_composer()
 
-    return {"Ola": "Mundo!"}
- 
+    response = await request_adapter(request, controller.handle)
+
+    return JSONResponse(
+        status_code=response['status_code'],
+        content={"data": response['data']}
+    )

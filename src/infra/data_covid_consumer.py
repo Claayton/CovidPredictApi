a2beb717f177ebcc9ed3610e3a966e4840321479
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-17 12:21:11 -0300

    feat: Implementado interface do novo metodo (get_data_covid_information) + remoçao de metodos de separaçao de dados, que serao implementados em casos de uso

--- src/infra/data_covid_consumer.py
+++ src/infra/data_covid_consumer.py
@@ -4,8 +4,7 @@ from typing import Dict, Tuple, Type
 from collections import namedtuple
 import requests
 from requests import Request
-from src.validators.countries_list import ALL_COUNTRIES
-from src.database.tables import CovidBrazil, CovidWorld, session, create_database_if_not_exist
+from src.database.tables import CovidBrazil, CovidWorld, session
 from src.errors import HttpRequestError
 from src.data.interfaces.data_covid_consumer import DataCovidConsumerInterface
 
@@ -54,6 +53,34 @@ class DataCovidConsumer(DataCovidConsumerInterface):
                 status_code=status_code
             )
 
+    def get_data_covid_information(self, country: str) -> Tuple[int, Type[Request], Dict]:
+        """
+        Realiza a requisição para a API de dados do covid,
+        retornando apenas os dados de um único país.
+        :param country: O país do qual deve ser retornado os dados
+        :return: Uma tupla com seus atributos (status_code, request, response).
+        """
+
+        request = requests.Request(
+            method='GET',
+            url=f'{self.url}',
+        )
+        request_prepared = request.prepare()
+
+        response = self.__send_http_request(request_prepared)
+        status_code = response.status_code
+
+        if not ((status_code >= 200) and (status_code <= 299)):
+            raise HttpRequestError(
+                message=response.json()['details'],
+                status_code=status_code
+            )
+        return self.get_data_covid_information_response(
+            status_code=status_code,
+            request=request,
+            response=response.json()[country]["data"]
+        )
+
     @classmethod
     def __send_http_request(cls, request_prepared: Type[Request]) -> any:
         """
@@ -66,137 +93,58 @@ class DataCovidConsumer(DataCovidConsumerInterface):
         response = http_session.send(request_prepared)
         return response
 
-    def get_data_covid_information(self, country: str) -> Tuple[int, Type[Request], Dict]:
-        """
-        Realiza a requisição para a API de dados do covid.
-        :return: Uma tupla com os atributos: (status_code, request, response).
-        """
+    # def register_data_from_brazil(self):
+    #     """
+    #     Registra os dados do brasil relacionados ao COvid-19 no banco de dados.
+    #     """
 
-        request = requests.Request(
-            method='GET',
-            url=f'{self.url}',
-        )
-        request_prepared = request.prepare()
+    #     brazil_data = self.separates_data_from_a_country("BRA")
 
-        response = self.__send_http_request(request_prepared)
-        status_code = response.status_code
+    #     for data in brazil_data:
+    #         new_day = CovidBrazil(
+    #             date=date.fromisoformat(data["date"]),
+    #             new_cases=int(data["new_cases"])
+    #         )
+    #         session.add(new_day)
+    #     session.commit()
 
-        if (status_code >= 200) and (status_code <= 299):
-            return self.get_data_covid_information_response(
-                status_code=status_code,
-                request=request,
-                response=response.json()[country]['data']
-            )
-        else:
-            raise HttpRequestError(
-                message=response.json()['details'],
-                status_code=status_code
-            )
+    # def register_data_from_world(self):
+    #     """
+    #     Registra os dados do mundo inteiro relacionados ao Covid-19 no banco de dados.
+    #     """
 
-    def separates_data_from_a_country(self, country: str) -> list:
-        """
-        Buscar dos dados da COVID-19 por cada país.
-        :param country: País no qual deve ser buscados os dados
-        :return: Uma lista de cada dia desde 26-02-2020,
-        separados por id, data e número de novos casos registrados no país.
-        """
-        response = self.get_data_covid()
-        days = response[country]["data"]
+    #     world_data_list = {}
+    #     world_data_by_country = self.separates_data_from_the_world()
+    #     world_data_list = world_data_by_country[0]
 
-        country_data_per_day = []
-        data = {}
+    #     for country in world_data_by_country:
+    #         for day in country:
 
-        for index, day in enumerate(days):
-            try:
-                data["id"] = index
-                data["date"] = day["date"]
-                data["new_cases"] = day["new_cases"]
-                country_data_per_day.append(data.copy())
-                data.clear()
-            except KeyError:
-                continue
-        return country_data_per_day
+    #             current_date = day["date"]
+    #             current_new_cases = day["new_cases"]
 
-    def separates_data_from_the_world(self):
-        """
-        Busca dados de todos os paises do mundo,
-        Realiza a soma dos casos de cada dia em todos os países do mundo
-        :return: Uma lista com os casos de cada dia no mundo.
-        """
-        world_data_by_country = []
-        country_data_per_day = []
-        data = {}
+    #             for day_world_list in world_data_list:
+    #                 if current_date == day_world_list["date"]:
+    #                     day_world_list["new_cases"] += current_new_cases
 
-        response = self.get_data_covid()
+    #     for data in world_data_list:
+    #         new_day = CovidWorld(
+    #             date=date.fromisoformat(data["date"]),
+    #             new_cases=int(data["new_cases"])
+    #         )
+    #         session.add(new_day)
+    #     session.commit()
 
-        for country in ALL_COUNTRIES:
-            data_by_country = response[country]["data"]
-            for index, day in enumerate(data_by_country):
-                try:
-                    data["id"] = index
-                    data["date"] = day["date"]
-                    data["new_cases"] = day["new_cases"]
-                    country_data_per_day.append(data.copy())
-                    data.clear()
-                except KeyError:
-                    continue
-            world_data_by_country.append(country_data_per_day[:])
-            country_data_per_day.clear()
+    # def read_data_from_brazil(self):
+    #     """
+    #     Realiza a leitura dos dados do Brasil no banco de dados.
+    #     """
+    #     query = session.query(CovidBrazil.date, CovidBrazil.new_cases).all()
+    #     return query
 
-        return world_data_by_country
-
-    def register_data_from_brazil(self):
-        """
-        Registra os dados do brasil relacionados ao COvid-19 no banco de dados.
-        """
-
-        brazil_data = self.separates_data_from_a_country("BRA")
-
-        for data in brazil_data:
-            new_day = CovidBrazil(
-                date=date.fromisoformat(data["date"]),
-                new_cases=int(data["new_cases"])
-            )
-            session.add(new_day)
-        session.commit()
-
-    def register_data_from_world(self):
-        """
-        Registra os dados do mundo inteiro relacionados ao Covid-19 no banco de dados.
-        """
-
-        world_data_list = {}
-        world_data_by_country = self.separates_data_from_the_world()
-        world_data_list = world_data_by_country[0]
-
-        for country in world_data_by_country:
-            for day in country:
-
-                current_date = day["date"]
-                current_new_cases = day["new_cases"]
-
-                for day_world_list in world_data_list:
-                    if current_date == day_world_list["date"]:
-                        day_world_list["new_cases"] += current_new_cases
-
-        for data in world_data_list:
-            new_day = CovidWorld(
-                date=date.fromisoformat(data["date"]),
-                new_cases=int(data["new_cases"])
-            )
-            session.add(new_day)
-        session.commit()
-
-    def read_data_from_brazil(self):
-        """
-        Realiza a leitura dos dados do Brasil no banco de dados.
-        """
-        query = session.query(CovidBrazil.date, CovidBrazil.new_cases).all()
-        return query
-
-    def read_data_from_world(self):
-        """
-        Realiza a leitura dos dados do mundo no banco de dados.
-        """
-        query = session.query(CovidWorld.date, CovidWorld.new_cases).all()
-        return query
+    # def read_data_from_world(self):
+    #     """
+    #     Realiza a leitura dos dados do mundo no banco de dados.
+    #     """
+    #     query = session.query(CovidWorld.date, CovidWorld.new_cases).all()
+    #     return query

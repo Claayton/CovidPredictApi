2758a9fdbd7969f04ddf249ac2a57a23a46bb42f
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-19 17:25:46 -0300

    feat: Implementado novo composer e novo controller para o usecase de information

---
+++ src/main/composers/get_data_covid_information_composer.py
@@ -0,0 +1,19 @@
+"""Diretório de inicialização e instâncias das classes"""
+from src.infra.data_covid_consumer import DataCovidConsumer
+from src.data.usecases.data_covid_information_colector import (
+    DataCovidInformationColector,
+)
+from src.presenters.controllers.data_covid_information_colector_controller import (
+    DataCovidInformationColectorController,
+)
+from src import config
+
+
+def get_data_covid_information_composer():
+    """Composer"""
+
+    infra = DataCovidConsumer(config.SEARCH_URL)
+    usecase = DataCovidInformationColector(infra)
+    controller = DataCovidInformationColectorController(usecase)
+
+    return controller
---
+++ src/presenters/controllers/data_covid_information_colector_controller.py
@@ -0,0 +1,26 @@
+"""Diretório controlador"""
+from typing import Dict, Type
+from src.presenters.interface.controllers import ControllersInterfaceInfo
+from src.domain.usecases.data_covid_information_colector import (
+    DataCovidInformationColectorInterface,
+)
+
+
+class DataCovidInformationColectorController(ControllersInterfaceInfo):
+    """Controller para DataCovidInformationColector"""
+
+    def __init__(
+        self,
+        data_covid_information_colector: Type[DataCovidInformationColectorInterface],
+    ) -> None:
+        self.__use_case = data_covid_information_colector
+
+    def handler(self, http_request: Dict) -> Dict:
+        """Handle para informações de controle de coletor"""
+
+        country = http_request["body"]["country"]
+        time = http_request["body"]["time"]
+
+        data_covid_information = self.__use_case.find_country(country, time)
+        http_response = {"status_code": 200, "data": data_covid_information}
+        return http_response

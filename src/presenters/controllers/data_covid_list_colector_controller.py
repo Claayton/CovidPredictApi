4f02d640dcf2d28132bbcb7b745975582aa8fd4e
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-19 17:23:00 -0300

    feat: Implementado Interfaces para as classes de controller

--- src/presenters/controllers/data_covid_list_colector_controller.py
+++ src/presenters/controllers/data_covid_list_colector_controller.py
@@ -1,19 +1,21 @@
 """Diretório controlador"""
 from typing import Dict, Type
+from src.presenters.interface.controllers import ControllersInterfaceList
 from src.domain.usecases.data_covid_list_colector import DataCovidListColectorInterface
 
 
-class DataCovidListColectorController:
+class DataCovidListColectorController(ControllersInterfaceList):
     """Controller para listas de dados covid"""
 
-    def __init__(self, data_covid_list_colector: Type[DataCovidListColectorInterface]) -> None:
+    def __init__(
+        self, data_covid_list_colector: Type[DataCovidListColectorInterface]
+    ) -> None:
         self.__use_case = data_covid_list_colector
 
-    def handle(self, http_request: Dict) -> Dict:
+    def handler(self) -> Dict:
         """Handler para listar coletores"""
 
-        country = [http_request['query_params']['country']]
-        data_covid_list = self.__use_case.list(country)
-        http_response = {'status_code': 200, 'data': data_covid_list}
+        data_covid_list = self.__use_case.list()
+        http_response = {"status_code": 200, "data": data_covid_list}
 
         return http_response

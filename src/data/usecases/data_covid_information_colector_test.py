8dd1f78157dc2c12b76dc2640de90c10621b1788
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-19 17:12:13 -0300

    test: Atualizaçao em alguns asserts dos tests de colector devido a Atualizaçoes dos retornos dos colectors

--- src/data/usecases/data_covid_information_colector_test.py
+++ src/data/usecases/data_covid_information_colector_test.py
@@ -1,6 +1,9 @@
 """Testes para data_covid_information_colector"""
-from src.data.usecases.data_covid_information_colector import DataCovidInformationColector
+from src.data.usecases.data_covid_information_colector import (
+    DataCovidInformationColector,
+)
 from src.infra.tests.data_covid_consumer import DataCovidConsumerSpy
+
 
 def test_find_country():
     """Testando p metodo find_country"""
@@ -8,12 +11,12 @@ def test_find_country():
     api_consumer = DataCovidConsumerSpy()
     data_covid_information_colector = DataCovidInformationColector(api_consumer)
 
-    country = 'BRA'
+    country = "BRA"
     time = 4
 
     response = data_covid_information_colector.find_country(country, time)
 
-    assert api_consumer.get_data_covid_information_attributes['country'] == country
-    assert isinstance(response, list)
-    assert 'new_cases_real' in response[0]
-    assert 'predicted_evolution' in response[0]
+    assert api_consumer.get_data_covid_information_attributes["country"] == country
+    assert isinstance(response, dict)
+    assert "new_cases_real" in response["data"][0]
+    assert "predicted_evolution" in response["data"][0]
--- src/data/usecases/data_covid_list_colector_test.py
+++ src/data/usecases/data_covid_list_colector_test.py
@@ -2,6 +2,7 @@
 from src.data.usecases.data_covid_list_colector import DataCovidListColector
 from src.infra.tests.data_covid_consumer import DataCovidConsumerSpy
 
+
 def test_list():
     """
     Testando o coletor da lista de dados covid no mundo.
@@ -9,10 +10,10 @@ def test_list():
     api_consumer = DataCovidConsumerSpy()
     data_covid_list_colector = DataCovidListColector(api_consumer)
 
-    country = ['BRA']
+    country = ["BRA"]
     response = data_covid_list_colector.list()
 
-    assert api_consumer.get_data_covid_attributes == {'country': country}
+    assert api_consumer.get_data_covid_attributes == {"country": country}
     assert isinstance(response, list)
-    assert "id" in response[0]
-    assert "new_cases" in response[0]
+    assert "country" in response[0]
+    assert "new_cases" in response[0]["data"][0]

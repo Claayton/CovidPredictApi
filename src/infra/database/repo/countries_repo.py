2199f2edccaf17459a77a6d0a51008552c619ed0
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-23 12:16:53 -0300

    test: Implementado testes para o metodo insert_country da classe CountryRepo

--- src/infra/database/repo/countries_repo.py
+++ src/infra/database/repo/countries_repo.py
@@ -1,5 +1,6 @@
 """Diretório de manipulação de dados"""
 from typing import List, Tuple
+from collections import namedtuple
 from src.infra.database.config import DataBaseConnectionHandler
 from src.infra.database.entities import Country
 
@@ -8,17 +9,21 @@ class CountryRepo:
     """Manipulação de dados da tabela Country"""
 
     @classmethod
-    def insert_country(cls, name: str) -> None:
+    def insert_country(cls, name: str) -> Country:
         """
         Realiza a inserção de um novo país na tabela Country.
         :param name: Nome ou abreviação do nome do país.
+        :return: Uma tupla nomeada com os todos os dados do novo país cadastrado.
         """
+
+        insert_data = namedtuple("Country", "id name")
 
         with DataBaseConnectionHandler() as data_base:
             try:
                 new_country = Country(name=name)
                 data_base.session.add(new_country)
                 data_base.session.commit()
+                return insert_data(id=new_country.id, name=new_country.name)
             except:
                 data_base.session.rollback()
                 raise

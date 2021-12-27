7e1dbc2affa762cc91a226696dd06991dcb02b51
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-27 12:39:18 -0300

    test: Implementado teste para o metodo update_cases da classe CovidCasesRepo

--- src/infra/database/repo/covid_cases_repo_test.py
+++ src/infra/database/repo/covid_cases_repo_test.py
@@ -10,7 +10,7 @@ data_base_connection_handler = DataBaseConnectionHandler()
 
 
 def test_insert_data():
-    """Testando o metodo insert_data"""
+    """Testando o método insert_data"""
 
     data_date = faker.date()
     new_cases = faker.random_number(digits=5)
@@ -26,5 +26,29 @@ def test_insert_data():
     assert new_covid_case.date == date.fromisoformat(query_covid_cases.date)
     assert new_covid_case.new_cases == query_covid_cases.new_cases
     assert new_covid_case.country_id == query_covid_cases.country_id
+
+    engine.execute(f"DELETE FROM covid_cases WHERE id='{new_covid_case.id}';")
+
+
+def test_update_cases():
+    """Testando o método update_cases"""
+
+    data_date = faker.date()
+    new_cases = faker.random_number(digits=5)
+    country = "ARG"
+
+    new_covid_case = covid_cases_repo.insert_data(data_date, new_cases, country)
+    update_covid_case = covid_cases_repo.update_cases(new_covid_case.id, 123)
+    engine = data_base_connection_handler.get_engine()
+    query_update_covid_case = engine.execute(
+        f"SELECT * FROM covid_cases WHERE id='{update_covid_case.id}';"
+    ).fetchone()
+
+    assert update_covid_case.id == query_update_covid_case.id
+    assert update_covid_case.id == new_covid_case.id
+    assert update_covid_case.date == new_covid_case.date
+    assert update_covid_case.country_id == new_covid_case.country_id
+    assert update_covid_case.new_cases == query_update_covid_case.new_cases
+    assert update_covid_case.new_cases != new_covid_case.new_cases
 
     engine.execute(f"DELETE FROM covid_cases WHERE id='{new_covid_case.id}';")

f22ed2da63d756bac02fd940e66972eb90886fbd
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-29 11:57:52 -0300

    feat: Implementado a classe GetCountries + Interface + tests

--- src/domain/usecases/__init__.py
+++ src/domain/usecases/__init__.py
@@ -1,3 +1,4 @@
 """Diretório de inicialização do módulo usecases"""
 from .data_covid_list_colector import DataCovidListColectorInterface
 from .register_country import RegisterCountryInterface
+from .get_countries import GetCountriesInterface

30173f4c17013945c606c82822d5a0a9961427e7
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-30 12:35:34 -0300

    test: Implementado tests para a classe GetCovidCases + mocks + Spy para a classe CovidCasesRepo

--- src/domain/tests/__init__.py
+++ src/domain/tests/__init__.py
@@ -1,2 +1,3 @@
 """Diretório de inicialização do módulo tests"""
 from .mock_countries import mock_countries
+from .mock_covid_cases import mock_covid_cases
--- src/infra/tests/__init__.py
+++ src/infra/tests/__init__.py
@@ -1,3 +1,4 @@
 """Diretório de inicialização do módulo tests"""
 from .countries_repo_spy import CountryRepoSpy
 from .data_covid_consumer import DataCovidConsumerSpy
+from .covid_cases_repo_spy import CovidCasesRepoSpy

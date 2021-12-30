b34a671928f21a453f58fbedb64fb84e93d71f74
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2021-12-30 12:34:21 -0300

    feat: Implementada caso de uso com a classe GetCovidCases + interface

--- src/domain/usecases/__init__.py
+++ src/domain/usecases/__init__.py
@@ -2,3 +2,4 @@
 from .data_covid_list_colector import DataCovidListColectorInterface
 from .register_country import RegisterCountryInterface
 from .get_countries import GetCountriesInterface
+from .get_covid_cases import GetCovidCasesInterface

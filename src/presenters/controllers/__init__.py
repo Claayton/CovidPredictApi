f11f96dd6d9c7511c17b12142b34978e2becb464
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2022-01-01 23:49:13 -0300

    feat: Implementado a classe RegisterCovidCasesController

--- src/presenters/controllers/__init__.py
+++ src/presenters/controllers/__init__.py
@@ -1,2 +1,3 @@
 """Diretório de inicialização do módulo controlers"""
 from .get_countries_controller import GetCountryController
+from .register_covid_cases_controller import RegisterCoviCasesController

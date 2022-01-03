cca0264f87af06e1cf63d5360feb0eba661f24e7
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2022-01-03 01:34:05 -0300

    feat: Atualizado a classe DataCovidConsumerSpy para ficar semelhante a classe original

--- src/infra/database/config/__init__.py
+++ src/infra/database/config/__init__.py
@@ -1,3 +1,3 @@
-"""Diretório de inicialização do módulo"""
+"""Diretório de inicialização do módulo config"""
 from .database_base import Base
 from .database_config import DataBaseConnectionHandler

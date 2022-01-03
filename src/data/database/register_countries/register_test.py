6bbf453048ecb55beb36aa998ca1b57a1f0952fa
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2022-01-03 02:02:32 -0300

    test: Atualizados testes para a classe RegisterCountry + Atualizado a classe RegisterCountrySpy

--- src/data/database/register_countries/register_test.py
+++ src/data/database/register_countries/register_test.py
@@ -1,38 +1,55 @@
 """Diretório de testes para a classe RegisterCountry"""
 from faker import Faker
-from src.infra.tests import CountryRepoSpy
+from src.infra.tests import CountryRepoSpy, DataCovidConsumerSpy
 from .register import RegisterCountry
 
 faker = Faker()
 
 
-def test_register():
-    """Testando o método register da classe RegisterCountry"""
+def test_register_country():
+    """Testando o método register_country da classe RegisterCountry"""
 
-    country_repo = CountryRepoSpy()
-    register_country = RegisterCountry(country_repo)
+    countries_repo = CountryRepoSpy()
+    data_covid_consumer = DataCovidConsumerSpy()
+    register_countries = RegisterCountry(countries_repo, data_covid_consumer)
 
-    attributes = {"name": faker.name()}
+    attributes = {"name": faker.name().upper()}
 
-    response = register_country.register(name=attributes["name"])
+    response = register_countries.register_country(name=attributes["name"])
 
-    assert country_repo.insert_country_params["name"] == attributes["name"]
+    assert countries_repo.insert_country_params["name"] == attributes["name"]
 
     assert response["success"] is True
-    assert response["data"]
+    assert response["data"] is not None
 
 
-def test_register_fail():
-    """Testando o erro no método register da classe RegisterCountry"""
+def test_register_country_fail():
+    """Testando o erro no método register_country da classe RegisterCountry"""
 
-    country_repo = CountryRepoSpy()
-    register_country = RegisterCountry(country_repo)
+    countries_repo = CountryRepoSpy()
+    data_covid_consumer = DataCovidConsumerSpy()
+    register_country = RegisterCountry(countries_repo, data_covid_consumer)
 
     attributes = {"name": faker.random_number(digits=2)}
 
-    response = register_country.register(name=attributes["name"])
+    response = register_country.register_country(name=attributes["name"])
 
-    assert country_repo.insert_country_params == {}
+    assert countries_repo.insert_country_params == {}
 
     assert response["success"] is False
     assert response["data"] is None
+
+
+def test_register_countries():
+    """Testando o método register_countries da classe RegisterCountry"""
+
+    countries_repo = CountryRepoSpy()
+    data_covid_consumer = DataCovidConsumerSpy()
+    register_countries = RegisterCountry(countries_repo, data_covid_consumer)
+
+    response = register_countries.register_countries()
+
+    assert countries_repo.insert_country_params is not None
+
+    assert response["success"] is True
+    assert response["data"] is not None
--- src/data/tests/register_country_spy.py
+++ src/data/tests/register_country_spy.py
@@ -1,19 +1,27 @@
 """Spy para a classe RegisterCountry"""
-from typing import Type, Dict
+from typing import Type, Dict, List
 from src.domain.models import Country
 from src.domain.usecases import RegisterCountryInterface
-from src.data.interfaces import CountryRepoInterface as CountryRepo
 from src.domain.tests import mock_countries
+from src.data.interfaces import (
+    CountryRepoInterface as CountryRepo,
+    DataCovidConsumerInterface as DataCovidConsumer,
+)
 
 
 class RegisterCountrySpy(RegisterCountryInterface):
     """Classe para definir o Spy para o caso de uso: RegisterCountry"""
 
-    def __init__(self, countries_repo: Type[CountryRepo]) -> None:
-        self.countries_repo = countries_repo
+    def __init__(
+        self,
+        countries_repo: Type[CountryRepo],
+        data_covid_consumer: Type[DataCovidConsumer],
+    ) -> None:
         self.name_params = {}
+        self.countries_repo = countries_repo
+        self.data_covid_consumer = data_covid_consumer
 
-    def register(self, name: str) -> Dict[bool, Country]:
+    def register_country(self, name: str) -> Dict[bool, Country]:
         """
         Registro de países no banco de dados.
         :param name: Abreviação do nome do país cadastrado.
@@ -27,5 +35,22 @@ class RegisterCountrySpy(RegisterCountryInterface):
 
         if validate_entry:
             response = mock_countries()
+
+        return {"success": validate_entry, "data": response}
+
+    def register_countries(self) -> Dict[bool, List[Country]]:
+        """
+        Registro de países no banco de dados.
+        :return: Um dicionário com as informações do processo.
+        """
+
+        countries = self.data_covid_consumer.get_countries().response
+        response = []
+        validate_entry = isinstance(countries, list)
+
+        if validate_entry:
+            for country in countries:
+                insersion = mock_countries()
+                response.append(insersion)
 
         return {"success": validate_entry, "data": response}

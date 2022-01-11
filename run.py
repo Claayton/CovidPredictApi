f98860db1d115feb3aca076b337f0ec9bb10fa9d
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2022-01-11 13:11:21 -0300

    feat: Implementado o padrão de projeto factory application

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 """Script para rodar o programa"""
 import uvicorn
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(
-        "src.main.config.http_server_configs:app",
-        host='0.0.0.0',
+        app="src.main.config.http_server_configs:create_app",
+        host="0.0.0.0",
         port=8000,
         reload=True,
-        debug=True
+        debug=True,
+        factory=True,
     )
--- src/main/config/http_server_configs.py
+++ src/main/config/http_server_configs.py
@@ -2,7 +2,13 @@
 from fastapi import FastAPI
 from src.main.routes import countries_routes, covid_cases_routes
 
-app = FastAPI()
 
-app.include_router(countries_routes)
-app.include_router(covid_cases_routes)
+def create_app() -> FastAPI:
+    """Função para criar o app"""
+
+    app = FastAPI()
+
+    app.include_router(countries_routes)
+    app.include_router(covid_cases_routes)
+
+    return app

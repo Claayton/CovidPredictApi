536460eac3c5bc45439d1a60d2fa052ed35d5626
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2022-01-30 12:33:13 -0300

    feat: Implementado composer para a rota register_covid_cases

--- src/main/composers/__init__.py
+++ src/main/composers/__init__.py
@@ -4,3 +4,4 @@ from .get_covid_cases_composer import get_covid_cases_composer
 from .covid_cases_colector_composer import covid_cases_colector_composer
 from .covid_cases_predict_composer import covid_cases_predict_composer
 from .register_countries_composer import register_countries_composer
+from .register_covid_cases_composer import register_covid_cases_composer

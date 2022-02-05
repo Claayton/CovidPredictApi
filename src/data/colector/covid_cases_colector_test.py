c1c62e804ec08860c2c10e5d7a14c1ba89d7477e
Clayton Garcia da Silva <claaytongarcia@gmail.com>
2022-02-05 15:41:37 -0300

    test: Atualizados os testes para o caso de uso covid_cases_colector

--- src/data/colector/covid_cases_colector_test.py
+++ src/data/colector/covid_cases_colector_test.py
@@ -1,8 +1,8 @@
 """Testes para a classe CovidCasesColector"""
-from src.data.colector import CovidCasesColector
 from src.infra.tests.data_covid_consumer_spy import DataCovidConsumerSpy
-from src.data.tests import GetCountrySpy
 from src.infra.database.repo import CountryRepo
+from src.data.colector import CovidCasesColector
+from src.data.tests import GetCountrySpy
 
 
 def test_covid_cases_country():
@@ -13,13 +13,19 @@ def test_covid_cases_country():
     get_countries = GetCountrySpy(countries_repo)
     covid_cases_colector = CovidCasesColector(api_consumer, get_countries)
 
-    country = "BRA"
-    days = 4
+    attribute = {"country": "BRA"}
 
-    response = covid_cases_colector.covid_cases_country(country, days)
+    response = covid_cases_colector.covid_cases_country(attribute["country"])
 
-    assert api_consumer.get_data_covid_by_country_attributes["country"] == country
+    api_consumer_attributes = api_consumer.get_data_covid_by_country_attributes[
+        "country"
+    ]
 
+    # Teste de entrada:
+    # Testando se os atributos enviados para a api_consumer são os mesmos enviados para o método.
+    assert api_consumer_attributes == attribute["country"]
+
+    # Teste de saída:
     assert isinstance(response, dict)
     assert isinstance(response["data"], list)
     assert isinstance(response["data"][0], dict)
@@ -28,19 +34,26 @@ def test_covid_cases_country():
 
 
 def test_covid_cases_country_error():
-    """Testando o erro no método covid_cases_country"""
+    """
+    Testando o erro no método covid_cases_country.
+    Enviando um número inteiro para o atributo country que deveria ser uma string.
+    """
 
     api_consumer = DataCovidConsumerSpy()
     countries_repo = CountryRepo()
     get_countries = GetCountrySpy(countries_repo)
     covid_cases_colector = CovidCasesColector(api_consumer, get_countries)
 
-    country = 5
-    days = 4
+    attributes = {"country": 5}
 
-    response = covid_cases_colector.covid_cases_country(country, days)
+    response = covid_cases_colector.covid_cases_country(attributes["country"])
 
-    assert api_consumer.get_data_covid_by_country_attributes == {}
+    api_consumer_attributes = api_consumer.get_data_covid_by_country_attributes
 
+    # Teste de entrada:
+    # Testando se os atributos enviados para a api_consumer são os mesmos enviados para o método.
+    assert api_consumer_attributes == {}
+
+    # Testando a saída
     assert response["success"] is False
     assert "error" in response["data"]
